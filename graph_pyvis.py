da845dae93f79d472de1a6581a562666df1cc49e
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2022-11-22 22:31:16 +0500

    SA: pyvis implementation

---
+++ graph_pyvis.py
@@ -0,0 +1,55 @@
+from baseGraph import BaseGraph
+from graph_networkx import NetworkX
+from pyvis.network import Network
+
+class Pyvis(NetworkX):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def drawGraph(self) -> None:
+        net = Network(height="98vh")
+        net.from_nx(self.graph)
+        net.toggle_physics(True)
+        pyvisOptions = """
+            options = {
+                "edges": {
+                    "arrows": {
+                        "to": {
+                            "enabled": true
+                        }
+                    },
+                    "color": {
+                        "inherit": false,
+                        "color": "#000000"
+                    },
+                    "font": {
+                        "strokeWidth": 0.1
+                    },
+                    "selfReferenceSize": null,
+                    "selfReference": {
+                        "angle": 0.7853981633974483
+                    },
+                    "smooth": {
+                        "forceDirection": "none"
+                    }
+                },
+                "physics": {
+                    "barnesHut": {
+                        "gravitationalConstant": -30000
+                    },
+                    "minVelocity": 0.75
+                }
+            }
+        """
+        net.set_options(pyvisOptions)
+        #net.show_buttons()
+        fileName = "graphs/Graph_Pyvis.html"
+        net.show(fileName)
+
+
+if (__name__ == "__main__"):
+    graph = Pyvis()
+    graph.readNetworkJson()
+    graph.createGraph()
+    graph.drawGraph()
+
