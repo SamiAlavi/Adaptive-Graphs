d30f096c54663cedc836531db816a277e76afdac
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2022-11-24 22:13:15 +0500

    SA: print file name in pylint

--- pylint.py
+++ pylint.py
@@ -4,6 +4,7 @@ import glob
 os.system('cmd /c')
 
 for fileName in  glob.glob('./**/*.py', recursive=True):
+    print(f'========== {fileName} ==========')
     os.system(f'\"pylint "{fileName}"\"')
 
 os.system('cmd /k')
