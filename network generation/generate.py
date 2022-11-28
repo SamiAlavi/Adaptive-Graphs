a436cac2a9efbe56e1afcb7d1f109a49a422c798
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2022-11-28 15:13:26 +0500

    SA: graphjson format

--- network generation/generate.py
+++ network generation/generate.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Generator, List, Tuple
 from string import ascii_uppercase
 import json
 import numpy as np
@@ -30,9 +30,7 @@ def generate_column_names(num_columns: int) -> List[str]:
     return titles
 
 def generate_matrix(num_nodes: int, max_connections_between_nodes: int) -> List[List[List[int]]]:
-    matrix = np.random.randint(0, 10, (num_nodes, num_nodes, max_connections_between_nodes))
-
-    return matrix
+    return np.random.randint(0, 10, (num_nodes, num_nodes, max_connections_between_nodes))
 
 def write_json(nodes: List[str], matrix: List[List[List[int]]], minify_json: bool) -> None:
     data = {
@@ -43,6 +41,56 @@ def write_json(nodes: List[str], matrix: List[List[List[int]]], minify_json: boo
 
     with open("network.json", "w", encoding="utf-8") as file:
         json.dump(data, file, indent=indent)
+
+def get_graph_edges(nodes: List[str], matrix: List[List[List[int]]]) -> Generator[Tuple[str, str, int], None, None]:
+    num_nodes = len(nodes)
+    for node_a_index in range(num_nodes):
+        node_a = nodes[node_a_index]
+        for node_b_index in range(num_nodes):
+            node_b = nodes[node_b_index]
+            weights = matrix[node_a_index][node_b_index]
+            for weight in weights:
+                if (weight > 0):
+                    yield node_a, node_b, weight
+
+def create_graphjson(nodes: List[str], matrix: List[List[List[int]]], minify_json: bool) -> None:
+    graph_json = {
+        "graph": {
+            "directed": True,
+            "type": "graph type",
+            "label": "graph label",
+            "metadata": {
+                "user-defined": "values"
+            },
+            "nodes": {},
+            "edges": []
+        }
+    }
+    for node in nodes:
+        graph_json["graph"][node] = {
+            "label": f"node label({node})",
+            "metadata": {
+                "type": "node type",
+                "user-defined": "values"
+            }
+        }
+    for node_a, node_b, weight in get_graph_edges(nodes, matrix):
+        edge = {
+            "source": f"{node_a}",
+            "relation": "edge relationship",
+            "target": f"{node_b}",
+            "directed": True,
+            "label": f"edge weight({weight})",
+            "metadata": {
+                "user-defined": "values"
+            }
+        }
+        graph_json["graph"]["edges"].append(edge)
+
+    indent = None if minify_json else 4
+
+    with open("network_graphjson.json", "w", encoding="utf-8") as file:
+        json.dump(graph_json, file, indent=indent)
 
 def print_example(nodes: List[str], matrix: List[List[List[int]]]) -> None:
     node_a_index = 0
@@ -58,6 +106,7 @@ def generate_dataset(num_nodes: int, max_connections_between_nodes: int, minify_
     nodes = generate_column_names(num_nodes)
     matrix = generate_matrix(num_nodes, max_connections_between_nodes)
     write_json(nodes, matrix, minify_json)
+    create_graphjson(nodes, matrix, minify_json)
     print_example(nodes, matrix)
 
 if (__name__ == "__main__"):
